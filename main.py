672022cbbbbd376f530028a8f0f766515103b036
himzei <himzei@gmail.com>
2025-02-07 13:31:02 +0900

    re

---
+++ main.py
@@ -0,0 +1,40 @@
+import csv
+from flask import Flask, render_template, request
+from scrapper import search_incruit
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html", title="대구한의대")
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    
+    # 실제 스그레이핑 
+    # jobs_incruit = search_incruit(keyword, 1)
+
+    # 임시 csv 파일을 불어오는 부분
+    with open("to_save.csv", "r", encoding="cp949") as file:
+        csv_reader = csv.reader(file)
+
+        jobs = []
+        for row in csv_reader:
+            job = {
+                "company_name": row[1],
+                "title": row[2],
+                "location": row[3], 
+                "link": row[4]
+            }
+
+            jobs.append(job)
+
+    return render_template(
+        "search.html", 
+        keyword=keyword, 
+        jobs=enumerate(jobs[1:])
+    )
+
+app.run()
---
+++ scrapper.py
@@ -0,0 +1,42 @@
+import requests 
+from bs4 import BeautifulSoup
+
+def search_incruit(keyword, page):
+
+    jobs = []
+
+    for i in range(page): 
+
+        page_no = i * 30
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno={page_no}"
+
+        headers = {
+            "User-Agent": 
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+        }
+        response = requests.get(url, headers=headers)
+        # print(response.status_code)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        lis = soup.find_all("li", class_="c_col")
+        for li in lis:
+            company_name = li.find("a", class_="cpname").text
+            title = li.select_one("div.cell_mid > div.cl_top").get_text(strip=True)
+            link = li.select_one("div.cell_mid > div.cl_top").find("a").get("href")
+            location = li.select_one("div.cell_mid > div.cl_md").find_all("span")[2].get_text(strip=True).replace(">", " ")
+
+            job = {
+                "company_name": company_name, 
+                "title": title, 
+                "link": link, 
+                "location": location
+            }
+
+            jobs.append(job)
+
+        print(f"{i+1}번째 페이지 스크래핑을 완료 했습니다.")
+    
+    return jobs
+
+
+
